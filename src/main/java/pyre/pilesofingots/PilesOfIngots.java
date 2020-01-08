d9d65c1b9fb560ffb8e17c157ec8d6a59cdec10d
Kamil Gronowski <kamil540@gazeta.pl>
2020-01-09 00:00:48 +0100

    Basic setup

---
+++ src/main/java/pyre/pilesofingots/PilesOfIngots.java
@@ -0,0 +1,32 @@
+package pyre.pilesofingots;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(PilesOfIngots.MODID)
+public class PilesOfIngots {
+    public static final String MODID = "pilesofingots";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public PilesOfIngots() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+        }
+    }
+}
